6eb553584e07c89013e10257f7997914f8e175b9
Sirazulhaq Bumpy <sirazulhaq0205@gmail.com>
2022-06-20 02:36:07 +0800

    better readable version of button room

--- admin/indexcopy.php
+++ admin/indexcopy.php
@@ -6,7 +6,6 @@ session_start();
 if (!isset($_SESSION['username'])) {
   header('Location: ../');
 }
-
 ?>
 <?php
 
@@ -166,41 +165,48 @@ if($bavail>0){
         <?php endfor ?>
         <?php endif ?>
         </form> 
-        <?php if(isset($_POST['room'])){
-        $room=$_POST['room'];
-    } ?>
+        
 <?php endif ?>
+
+
 
 
 
 <?php if(isset($_POST['room'])&& $a1[$_POST['room']]=='green'):?>
   <?php 
   $roomid="A0".str_pad($_POST['room'],2,0,STR_PAD_LEFT);
+  if(isset($roomid)){
+    $_SESSION['room_id']=$roomid;
+  }
   $query1="SELECT * FROM bed WHERE room_id='$roomid' ORDER BY bed_id,availability ";
-  echo $query1;
   $result1 = mysqli_query($dbconn, $query1) or die ("Error: " . mysqli_error($dbconn));
-  $numrow1 = mysqli_num_rows($result1);
-  $id=$_SESSION['user_id'];
-  $query2='INSERT INTO reserve (user_id,bed_id) VALUES ("$id","$roomid") ';?>
+  $numrow1 = mysqli_num_rows($result1);?>
   <form method="POST">
   <?php for ($a=0;$a<$numrow1;$a++):
       $row1 = mysqli_fetch_array($result1);
       if($row1['availability']==1):?>
-          <input type="submit" value=<?php echo $a+1 ?> style='background-color:green;' id='bedavail' name='bedavail'>
+          <input type="submit" value=<?php echo $a+1 ?> style='background-color:green;' id='ba' name='bedavail'>
       <?php endif ?>
       <?php if($row1['availability']==0):?>
           <input type="submit" value=<?php echo $a+1 ?> style='background-color:red;' id='bed' name='bed'>
       <?php endif ?>
   <?php endfor ?>
-  </form>                 
+  </form>
+  <?php endif ?>   
+  <?php echo $_SESSION['room_id'];?>           
   <?php if(isset($_POST['bedavail'])):?>
-      <?php echo 'hi'; ?>
-      //$result1 = mysqli_query($dbconn, $query2) or die ("Error: " . mysqli_error($dbconn));
+    <?php 
+        $id=$_SESSION['user_id'];
+        $roomid= $_SESSION['room_id'];
+        $bedid=$roomid.$_POST['bedavail'];
+        $query2="INSERT INTO reserve (user_id,bed_id) VALUES ('$id','$bedid') ";
+        $result1 = mysqli_query($dbconn, $query2) or die ("Error: " . mysqli_error($dbconn));
+    ?>
+      
   <?php endif ?>
-<?php endif ?>
 
 
-
+  
 
 
 
